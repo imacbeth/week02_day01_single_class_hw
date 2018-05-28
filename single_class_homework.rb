8eee592be5fb90e3734d93ba6af7eee1c97ad0fd
Iona Macbeth <ionalmacbeth@gmail.com>
2018-05-28 16:34:32 +0100

    Part A complete

--- single_class_homework.rb
+++ single_class_homework.rb
@@ -1,22 +1,33 @@
-# Classes Lab / Homework
+class Student
 
-In this lab, we'd like you to make some classes of your own. Below we've outlined a few scenarios to be modelled as classes. Throughout each part stick to using our Test Driven Development process of writing our tests first then writing our methods and refactoring until they pass. Make sure you create two files for each class: one test file and one class file (do not put different classes in the same file).
+  def initialize(student_name, cohort, language)
+    @student_name = student_name
+    @cohort = cohort
+    @language = language
+  end
 
-## Learning Objectives
+  def student_name
+    return @student_name
+  end
 
-1. Create your own class
-2. Create multiple objects
-3. Add some properties
-4. Use accessors
-5. Add some methods and behaviours
-6. Get methods and behaviours to interact with properties
+  def cohort
+    return @cohort
+  end
 
-### Part A
+  def set_student_name(input_name)
+    @student_name = input_name
+  end
 
-For this part we want you to make a class that represents a CodeClan student.
+  def set_cohort(input_cohort)
+    @cohort = input_cohort
+  end
 
-* Create a class called Student that takes in a name (String) and a cohort (string - e.g "E18", "G6", etc) when an new instance is created.
-* Create a couple of Getter methods, one that returns the name property and one that returns the cohort property of the student.
-* Add in Setter methods to update the students name and what cohort they are in.
-* Create a method that gets the student to talk (eg. Returns "I can talk!).
-* Create a method that takes in a students favourite programming language and returns it as part of a string (eg. `student1.say_favourite_language("Ruby")` -> "I love Ruby").
+  def can_talk()
+    return "I can talk!"
+  end
+
+  def say_favourite_language()
+    return "I love #{@language}!"
+  end
+
+end
